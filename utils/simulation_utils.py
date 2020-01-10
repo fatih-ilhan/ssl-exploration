49187dba706453e3bf466bb64083ae1c6ae1b706
fi <fatihilhan66@gmail.com>
2020-01-10 15:52:59 +0300

    feature simulation added

--- utils/simulation_utils.py
+++ utils/simulation_utils.py
@@ -1,6 +1,11 @@
 import os
 import random
 import numpy as np
+from sklearn import decomposition
+from sklearn.cluster import KMeans
+from sklearn.metrics import pairwise_distances
+
+import config
 
 
 def simulate_missing_labels(x, y, params):
@@ -35,7 +40,35 @@ def simulate_missing_labels_class(x, y, class_p):
     return x, y
 
 
-def simulate_missing_labels_feature(x, y):
+def simulate_missing_labels_feature(x, y, feature_p):
+    x_ = x.copy()
+    y_ = y.copy()
+
+    PCA = decomposition.PCA(whiten=True)
+    PCA.n_components = x.shape[1]
+    PCA.fit(x)
+
+    n_components = np.where(PCA.explained_variance_ratio_.cumsum() > 0.95)[0][0]
+    PCA = decomposition.PCA(n_components=n_components, whiten=True)
+
+    PCA.fit(x)
+    x = PCA.transform(x)
+
+    num_classes = y.max() + 1
+    kmeans = KMeans(n_clusters=num_classes, random_state=0).fit(x)
+    centers = kmeans.cluster_centers_
+
+    diff = pairwise_distances(x, centers)
+    a = np.min(diff, axis=1)
+    b = np.sort(diff, axis=1)[:, 1]
+    diff = (b - a) / b
+
+    idx_sorted = np.argsort(diff)
+    num_data_to_miss = int(x.shape[0] * (1 - feature_p))
+    idx_to_miss = idx_sorted[:num_data_to_miss]
+
+    y[idx_to_miss] = -1
+
     return x, y
 
 
